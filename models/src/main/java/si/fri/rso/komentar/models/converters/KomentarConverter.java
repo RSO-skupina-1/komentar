c737e6e20301084243fdbfe33b4b7e992ec67828
grzin <36706979+grzin@users.noreply.github.com>
2023-12-04 20:45:08 +0100

    init

---
+++ models/src/main/java/si/fri/rso/komentar/models/converters/KomentarConverter.java
@@ -0,0 +1,36 @@
+package si.fri.rso.komentar.models.converters;
+
+import si.fri.rso.komentar.lib.Komentar;
+import si.fri.rso.komentar.models.entities.KomentarEntity;
+
+public class KomentarConverter {
+
+    public static Komentar toDto(KomentarEntity entity) {
+
+        Komentar dto = new Komentar();
+        dto.setUstvarjen(entity.getUstvarjen());
+        dto.setKomentar(entity.getKomentar());
+        dto.setOcena(entity.getOcena());
+        dto.setLokacija_id(entity.getLokacija_id());
+        dto.setUser_id(entity.getUser_id());
+
+
+        return dto;
+
+    }
+
+    public static KomentarEntity toEntity(Komentar dto) {
+
+        KomentarEntity entity = new KomentarEntity();
+        entity.setUstvarjen(dto.getUstvarjen());
+        entity.setKomentar(dto.getKomentar());
+        entity.setOcena(dto.getOcena());
+        entity.setLokacija_id(dto.getLokacija_id());
+        entity.setUser_id(dto.getUser_id());
+
+
+        return entity;
+
+    }
+
+}
---
+++ models/src/main/resources/sql-scripts/init.sql
@@ -0,0 +1,1 @@
+INSERT INTO komentar_metadata (user_id, lokacija_id, komentar, ocena, ustvarjen) VALUES (1, 1, 'Kranjska Gora je super!', 5, TIMESTAMP '2019-01-01 00:00:00');
