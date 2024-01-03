f6cd81a442ab2a4d9493850ad0874a4ebd217816
grzin <36706979+grzin@users.noreply.github.com>
2024-01-03 21:51:56 +0100

    Revert "Update KomentarResource.java"
    This reverts commit d11bcdfc7cd1b1bbaa58254c9b8fea69b28afe47.

--- api/src/main/java/si/fri/rso/priporocilni/api/v1/resources/PriporocilniResource.java
+++ api/src/main/java/si/fri/rso/priporocilni/api/v1/resources/PriporocilniResource.java
@@ -193,8 +193,6 @@ public class KomentarResource {
 
         // kdaj dobim exception Internal Exception: org.postgresql.util.PSQLException: ERROR: prepared statement "S_2" already exists
         // bi bilo idealno za error prevention.
-        // nov komentar, da lahko commitam
-        
         return Response.status(Response.Status.CREATED).entity(komentarBean.createKomentar(komentar)).build();
     }
 
